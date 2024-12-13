883da8e9f2a8b20b7f2ba732ab0b99b25610936c
ayhamrabea <ayhamrabea05@.com>
2024-12-13 11:03:24 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay:true,
+    loop:true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
