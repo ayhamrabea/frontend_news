3a51a52069136d298f07b4aab51de1908372542c
ayhamrabea <ayhamrabea05@.com>
2024-12-18 15:20:45 +0300

    page respons

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
-var swiper = new Swiper(".swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    autoplay:true,
-    loop:true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+const mql = window.matchMedia("(max-width: 992px)");
+const changSlideView = (slide) =>{
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: slide,
+        spaceBetween: 30,
+        autoplay:true,
+        loop:true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+}
+
+const heandelOientationChange = (e) => {
+    if (mql.matches) {
+        changSlideView(1);
+    } else {
+        changSlideView(3);
+    }
+}
+
+
+
+heandelOientationChange();
+mql.onchange = (e) =>{
+    heandelOientationChange(e);
+}
+
 
 
 document.getElementById("year").innerHTML = new Date().getFullYear()
